dda2add0d030eef19347ff12a7bf69871975fab9
Yossi Tzeel-Krupp <yossitz1302@gmail.com>
2023-02-15 13:11:13 +0200

    added slide show in popup

--- js/main.js
+++ js/main.js
@@ -141,3 +141,11 @@ function alert() {
         x.style.display = "none";
     }
 }
+
+$("#Next").click(function(){
+	$("#Slider").append($("#Slider img:first-of-type"));
+});
+
+$("#Prev").click(function(){
+	$("#Slider").prepend($("#Slider img:last-of-type"));
+});
